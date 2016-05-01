a13cd1f2b0c110467f9d9a84ed610c0d35e134ac
javelinsman <javelinsman@snu.ac.kr>
2016-05-01 11:07:29 -0700

    dm hw4

--- data_mining/hw/hw4/q4.cpp
+++ data_mining/hw/hw4/q4.cpp
@@ -16,7 +16,7 @@ double M[4][4] = {
 double r[4] = { 1.0/4.0, 1.0/4.0, 1.0/4.0, 1.0/4.0 }, prev_r[4];
 double temp[4];
 
-bool is_telset[4] = { true, true, true, true };
+bool is_telset[4] = {true,true,true,true};
 int tn = 4;
 
 void precalc(){
@@ -49,7 +49,7 @@ int main(){
         for(int k=0;k<n;k++){
             err += fabs(r[k] - prev_r[k]);
         }
-        printf("[iter %d] r = (%.7f, %.7f, %.7f, %.7f) with err = %.7f\n",
+        printf("[iter %d] r = (%.16f, %.16f, %.16f, %.16f) with err = %.7f\n",
                 ii + 1, r[0], r[1], r[2], r[3], err);
     }
     return 0;
